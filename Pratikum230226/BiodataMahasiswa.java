e115a6116d51182d975afba28e1a71b72a33b777
Khairunnisah <khrunnsah2605@gmail.com>
2026-03-01 22:26:41 +0700

    Add files via upload

---
+++ Pratikum230226/BiodataMahasiswa.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pratikum230226;
+
+/**
+ *
+ * @author Khairunnisah
+ */
+public class BiodataMahasiswa {
+    public static void main(String[]args){
+        //nama, nobp, prodi, kelas, asal sma, alamat di padang
+        //nama orang tua, alamat orang tua, no hp orang tua
+        System.out.println("Nama\t\t: Nisa");
+        System.out.println("NoBP\t\t: 2501082018");
+        System.out.println("Prodi\t\t: D3 Teknik Komputer");
+        System.out.println("Kelas\t\t: 1B");
+        System.out.println("Asal SMA\t: SMA 1 BATIPUH");
+        System.out.println("Alamat \t\t: Jln. Pasar Baru No 5 Padang");
+        System.out.println("NoHP\t\t: ");
+        System.out.println("Nama Ortu\t: Ayahnya ");
+        System.out.println("Alamat Ortu\t: Batipuh Selatan, Tanah Datar");
+        System.out.println("NoHP Ortu\t: ");
+        
+    }
+    
+}
