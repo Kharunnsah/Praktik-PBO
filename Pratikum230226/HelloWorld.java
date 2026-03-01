05486ca944dbb01597bf2b877be17e8ce5be7a6f
Khairunnisah <khrunnsah2605@gmail.com>
2026-03-01 22:08:30 +0700

    Add files via upload

---
+++ Pratikum230226/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pratikum230226;
+
+/**
+ *
+ * @author Khairunnisah
+ */
+public class HelloWorld {
+    public static void main (String[]args){
+        System.out.println("Hello World");
+        System.out.println("Teknik Komputer 1B");
+    }
+    
+}
