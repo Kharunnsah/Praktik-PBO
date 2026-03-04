a34079b33109131b58f671fbdcfb4e2c0e9b0207
Khairunnisah <khrunnsah2605@gmail.com>
2026-03-04 11:56:22 +0700

    Add files via upload

---
+++ Pratikum230226/InputAngka.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pratikum230226;
+
+/**
+ *
+ * @author Khairunnisah
+ */
+import java.util.Scanner;
+public class InputAngka {
+      public static void main (String[] args){
+        int x;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukkan angka : ");
+        x = input.nextInt(); ///baca input
+        System.out.println("angka = "+ x);
+    }
+    
+}
