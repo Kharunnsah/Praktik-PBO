566a411db2f470fb9becab1d816204f5074af994
Khairunnisah <khrunnsah2605@gmail.com>
2026-06-11 09:02:54 +0700

    Add files via upload

---
+++ Pratikum110626/CekAngka.java
@@ -0,0 +1,41 @@
+/*
+Nama : Khairunnisah
+Nim  : 25010082018
+ */
+package Pratikum110626;
+
+/**
+ *
+ * @author Khairunnisah
+ */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class CekAngka {
+    public static void main(String[]args){
+        // TODO code application logic here 
+        Scanner input = new Scanner (System.in);
+        int a,b;
+        int hasil;
+        try{
+        System.out.print("Masukkan bilangan pertama : ");
+        a = input.nextInt();
+        System.out.print("Masukkan bilangan kedua : ");
+        b = input.nextInt();
+        hasil = a/b;
+        System.out.println(a+"/"+b+" ="+hasil );
+        //jika tidak tau penggunaan exception cukup bikin saja exception e + e message
+        }
+        catch(InputMismatchException e){
+            System.out.println("Masukkan nilai berupa angka!");
+        }
+        catch(ArithmeticException e){
+            System.out.println("nilai ke 2/penyebut tidak boleh 0!");
+        }
+        catch(Exception e){
+            System.out.println("Error tidak terduga"+ e.getMessage());
+                }    
+        finally{
+            System.out.println("Pengecekan nilai selesai");
+        }
+    }
+}                                                          
